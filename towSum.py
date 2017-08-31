94a3fd17eec1646c6d8c332bcdd17629f072eab5
GY <330336317@qq.com>
2017-08-31 10:36:14 +0800

    tow sum

---
+++ towSum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if n in buff_dict:
+                return [buff_dict[n], i]
+            else:
+                buff_dict[m] = i
+
+
+nums = [11, 2, 2, 7, 11, 15]
+target = 9
+solution = Solution()
+print (solution.twoSum(nums, target))
